282a52fa0fe5d45092c22ce073c800692e5db1b7
MingbinY <Mingbin.Yang@outlook.com>
2022-11-02 12:08:34 -0400

    Update Card.cs
    shorting order related functions

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -4,17 +4,18 @@ using System.Collections.Generic;
 
 public class Card : MonoBehaviour {
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
+	public string suit;
+	public int rank;
+	public Color color = Color.black;
+	public string colS = "Black";  // or "Red"
+
 	public List<GameObject> decoGOs = new List<GameObject>();
 	public List<GameObject> pipGOs = new List<GameObject>();
-	
+
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+	public SpriteRenderer[] spriteRenderers;
 
 	public bool faceUp {
 		get {
@@ -28,13 +29,56 @@ public class Card : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		SetSortOrder(0);
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update() {
+
+	}
+
+	public void PopulateSpriteRenderers()
+	{
+		if (spriteRenderers == null || spriteRenderers.Length == 0)
+		{
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	public void SetSortingLayerName(string tSLN)
+	{
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder(int sOrd)
+	{
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			if (tSR.gameObject == this.gameObject)
+            {
+				tSR.sortingOrder = sOrd;
+				continue;
+            }
+
+			switch (tSR.gameObject.name)
+            {
+				case "back":
+					tSR.sortingOrder = sOrd + 2;
+					break;
+				case "face":
+				default:
+					tSR.sortingOrder = sOrd + 1;
+					break;
+            }
+		}
 	}
 } // class Card
 
