d97597d94e3b989d1f1ff696c4435ec4f6e8d10d
MingbinY <Mingbin.Yang@outlook.com>
2022-11-02 12:26:10 -0400

    On Click functions

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -80,6 +80,11 @@ public class Card : MonoBehaviour {
             }
 		}
 	}
+
+	virtual public void OnMouseUpAsButton()
+    {
+		print(name);
+    }
 } // class Card
 
 [System.Serializable]
--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -7,17 +7,17 @@ using System.Collections.Generic;
 
 public class Prospector : MonoBehaviour {
 
-	static public Prospector 	S;
+	static public Prospector S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
 	public TextAsset layoutXML;
 	public float xOffset = 3;
 	public float yOffset = -2.5f;
 	public Vector3 layoutCenter;
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck deck;
 	public Layout layout;
 	public List<CardProspector> drawPile;
 	public Transform layoutAnchor;
@@ -25,14 +25,14 @@ public class Prospector : MonoBehaviour {
 	public List<CardProspector> tableau;
 	public List<CardProspector> discardPile;
 
-	void Awake(){
+	void Awake() {
 		S = this;
 	}
 
 	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
-		Deck.Shuffle (ref deck.cards);
+		deck = GetComponent<Deck>();
+		deck.InitDeck(deckXML.text);
+		Deck.Shuffle(ref deck.cards);
 
 		//Card c;
 		//for (int cNum = 0; cNum < deck.cards.Count; cNum++)
@@ -61,25 +61,25 @@ public class Prospector : MonoBehaviour {
 	}
 
 	CardProspector Draw()
-    {
+	{
 		CardProspector cd = drawPile[0];
 		drawPile.RemoveAt(0);
 		return cd;
-    }
+	}
 
 	void LayoutGame()
-    {
+	{
 		if (layoutAnchor == null)
-        {
+		{
 			GameObject tGO = new GameObject("_LayoutAnchor");
 			layoutAnchor = tGO.transform;
 			layoutAnchor.transform.position = layoutCenter;
-        }
+		}
 
 		CardProspector cp;
 
 		foreach (SlotDef tSD in layout.slotDefs)
-        {
+		{
 			cp = Draw();
 			cp.faceUp = tSD.faceUp;
 			cp.transform.parent = layoutAnchor;
@@ -90,6 +90,76 @@ public class Prospector : MonoBehaviour {
 			cp.state = eCardState.tableau;
 			cp.SetSortingLayerName(tSD.layerName);
 			tableau.Add(cp);
+		}
+		MoveToTarget(Draw());
+		UpdateDrawPile();
+	}
+
+	void MoveToDiscard(CardProspector cd)
+    {
+		cd.state = eCardState.discard;
+		discardPile.Add(cd);
+		cd.transform.parent = layoutAnchor;
+
+		cd.transform.localPosition = new Vector3
+			(layout.multiplier.x * layout.discardPile.x, layout.multiplier.y * layout.discardPile.y, -layout.discardPile.layerID + 0.5f);
+		cd.faceUp = true;
+		cd.SetSortingLayerName(layout.discardPile.layerName);
+		cd.SetSortOrder(-100+discardPile.Count);
+    }
+
+	void MoveToTarget(CardProspector cd)
+    {
+		if (target != null)
+        {
+			MoveToDiscard(target);
+        }
+		target = cd;
+		cd.state = eCardState.target;
+		cd.transform.parent = layoutAnchor;
+
+		cd.transform.localPosition = new Vector3
+			(layout.multiplier.x * layout.discardPile.x, layout.multiplier.y * layout.discardPile.y, -layout.discardPile.layerID);
+		cd.faceUp = true;
+		cd.SetSortingLayerName(layout.discardPile.layerName);
+		cd.SetSortOrder(0);
+    }
+
+	void UpdateDrawPile()
+    {
+		CardProspector cd;
+
+		for (int i = 0; i < drawPile.Count; i++)
+        {
+			cd = drawPile[i];
+			cd.transform.parent = layoutAnchor;
+
+			Vector2 dpStagger = layout.drawPile.stagger;
+			cd.transform.localPosition = new Vector3
+				(layout.multiplier.x * (layout.drawPile.x + i * dpStagger.x),
+				layout.multiplier.y * (layout.drawPile.y + i * dpStagger.y),
+				-layout.drawPile.layerID + 0.1f * i
+				);
+			cd.faceUp = false;
+			cd.state = eCardState.drawpile;
+			cd.SetSortingLayerName(layout.drawPile.layerName);
+			cd.SetSortOrder(-10 * i);
+        }
+    }
+
+	public void CardClicked(CardProspector cd)
+    {
+		switch (cd.state)
+        {
+			case eCardState.target:
+				break;
+			case eCardState.drawpile:
+				MoveToDiscard(target);
+				MoveToTarget(Draw());
+				UpdateDrawPile();
+				break;
+			case eCardState.tableau:
+				break;
         }
     }
 }
